392917016e498d7c51d2c6bde6b88990b09e3fef
fortym2 <davide@fortyzone.it>
2018-06-10 17:57:34 +0200

    Little graphical refinements, prevent crash on inexistent option given

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let skippedAnswers = 0;
 
 let index = 0;
 for (const question in questions) {
-    console.log(++index + ". " + question);
+    console.log('\x1b[36m%s. %s\x1b[0m', ++index, question);
     
     let choiceIndex = 0;
     for (const choice in questions[question]) {
@@ -21,7 +21,8 @@ for (const question in questions) {
     }
     
     const answer = query("    Your answer: ");
-    handleAnswer(question, answer);
+    handleAnswer(question, answer, choiceIndex);
+    console.log("");
 }
 
 const score = correctAnswers*correctValue + wrongAnswers*wrongValue + skippedAnswers*skippedValue;
@@ -29,8 +30,8 @@ const maxScore = index*correctValue;
 console.log(`\n\nAnswers: ${index}\n${correctAnswers} correct\n${wrongAnswers} wrong\n${skippedAnswers} skipped`);
 console.log(`Your score is ${score}/${maxScore} ≈ ${(score/maxScore*100).toFixed(2)}%`);
 
-function handleAnswer(question, answer) {
-    if (answer === 0) {
+function handleAnswer(question, answer, maxChoices) {
+    if (answer === 0 || answer > maxChoices) {
         console.log('    SKIPPED');
         skippedAnswers++;
         return;
